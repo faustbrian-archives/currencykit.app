80b1a6ab1ce9be744a15c471e50d35464f4f021f
Brian Faust <faustbrian@users.noreply.github.com>
2020-10-06 15:40:01 +0300

    add `applyFilters` method

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    protected function mapRates($rates): Collection
+    {
+        return $rates->mapWithKeys(fn ($rate) => [$rate['currency'] => floatval($rate['rate'])]);
+    }
+
+    protected function applyFilters($request, $rates): Collection
+    {
+        if ($request->has('base')) {
+            $base = $request->get('base');
+
+            if ($rates->keys()->contains($base)) {
+                $baseRate = $rates->get($base);
+
+                $rates->transform(fn ($rate) => round($rate / $baseRate, 4));
+
+                if ($base !== 'EUR') {
+                    $rates->put('EUR', round(1 / $baseRate, 4));
+                }
+            }
+        }
+
+        // if ($request->has('symbols')) {
+        //     $rates = $rates->filter(fn ($rate, $currency) => \in_array($currency, explode(',', $request->get('symbols')), true));
+        // }
+
+        return $rates->sortKeys();
+    }
 }
